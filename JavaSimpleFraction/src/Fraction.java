9381b972b1d12e87b60f9181b27e5eac00b09c18
Tristan <tristan.bessac@live.fr>
2018-10-01 14:24:59 +0200

    correct on previous commit

--- JavaSimpleFraction/src/Fraction.java
+++ JavaSimpleFraction/src/Fraction.java
@@ -1,5 +1,18 @@
 
 public class Fraction {
-	
+
+    private int num;
+    private int den;
+
+    public Fraction(int num, int den) {
+        this.num = num;
+        this.den = den;
+    }
+
+    @Override
+    public String toString() {
+        return this.num + "/" + this.den + " = " + (float)this.num/this.den;
+    }
+
 
 }
